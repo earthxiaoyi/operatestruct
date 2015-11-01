2e9d6466bf59a610199094a469c43191cba57380
JM <JM@192.168.233.1>
2015-11-01 14:34:08 +0800

    zengjia

--- src/main/java/com/operatestructs/linklist2/DoubleLinkList.java
+++ src/main/java/com/operatestructs/linklist2/DoubleLinkList.java
@@ -1,5 +1,7 @@
 package com.operatestructs.linklist2;
 
+import com.operatestructs.linklist.LinkNode;
+
 public class DoubleLinkList {
 	
 	private Node2 last;
@@ -61,8 +63,19 @@ public class DoubleLinkList {
 	}
 	
 	public boolean insertAfter(int key,int id){
-		
-		return false;
+		//1：找到要在其后添加新节点的节点
+		Node2 node = find(key);
+		Node2 newLink = new Node2(id); 
+		if(node.getId()==last.getId()){
+			newLink.setNext(null);
+			last = newLink;
+		}else{//2：分别设置这3个节点：要在其后添加新节点的节点、新节点、要在其后添加新节点的后继结点
+			newLink.setNext(node.getNext());
+			node.getNext().setPrivious(newLink);
+		}
+		newLink.setPrivious(node);
+		node.setNext(newLink);
+		return true;
 	}
 	
 	/**
@@ -71,18 +84,36 @@ public class DoubleLinkList {
 	 * @return
 	 */
 	public Node2 remove(int id){
-		/**
-		 * 1:先找到删除的结点
-		 * 2:如果这个结点是first，那么就把它的next作为新的first
-		 * 2.1否则就把这个结点的privious结点的next，设置成为这个结点的next
-		 * 3:如果这个结点是last，那么就把它的privious结点置为last
-		 * 3.1 否则就把这个结点的next结点的privious结点，设置成为这个结点的privious
-		 */
-		
+		//1:先找到删除的结点
+		Node2 node = find(id);
+		//2:如果这个结点是first，那么就把它的next作为新的first
+		//2.1否则就把这个结点的privious结点的next，设置成为这个结点的next
+		if(node.getId()==first.getId()){
+			first.getNext().setPrivious(null);
+			first = first.getNext();
+		}else{
+			node.getPrivious().setNext(node.getNext());
+		}
+		//3:如果这个结点是last，那么就把它的privious结点置为last
+		//3.1 否则就把这个结点的next结点的privious结点，设置成为这个结点的privious
+		if(node.getId()==last.getId()){
+			last = last.getPrivious();
+		}else{
+			node.getNext().setPrivious(node.getPrivious());
+		}
 		return null;
 	}
 	
 	public Node2 find(int id){
-		return null;
+		Node2 node = first;
+		//循环递归查找
+		while(node.getId()!=id){
+			if(node.getNext()!=null){
+				node = node.getNext();
+			}else{
+				return null;
+			}
+		}
+		return node;
 	}
 }
